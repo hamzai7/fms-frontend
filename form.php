571d29c999fd48001f51772b0d16fe723bd2f382
hamzai7 <hamzaiqbal.hi7@gmail.com>
2020-10-21 18:09:48 -0500

    Transferred version 1 files into the repository

---
+++ form.php
@@ -0,0 +1,78 @@
+<html>
+
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <h1>Form Page</h1>
+
+    <!-- Printer Reset Form -->
+    <hr>
+
+    <form class="form-horizontal" action="updateprinter.php">
+        <fieldset>
+
+            <!-- Form Name -->
+            <legend>To update the status of a printer please fill out the form below</legend>
+
+            <!-- Text input for selected printer-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="selectedprinter">Which printer are you updating the status of?</label>  
+                <div class="col-md-6">
+                    <input id="selectedprinter" name="selectedprinter" type="text" placeholder="e.g. Printer 01, Printer 23" class="form-control input-md">
+                    <span class="help-block">Enter the printer name as shown in the grid</span>  
+                </div>
+            </div>
+
+            <!-- Text input for new status-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="newstatus">What is the new status for this printer?</label>  
+                <div class="col-md-6">
+                    <input id="newstatus" name="newstatus" type="text" placeholder="e.g. Ready, Usable, Unusable, Busy" class="form-control input-md">
+                    <span class="help-block">Enter the new status here.</span>  
+                </div>
+            </div>
+
+            <!-- Text input for new user-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="newuser">Who is now using this printer?</label>  
+                <div class="col-md-5">
+                    <input id="newuser" name="newuser" type="text" placeholder="e.g. John Doe" class="form-control input-md">
+                    <span class="help-block">Enter your name if you are reserving this printer, otherwise enter "None" </span>  
+                </div>
+            </div>
+
+            <!-- Text input for new user-->
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="usetemp">What temperature was this printer used at last?</label>  
+                <div class="col-md-5">
+                    <input id="usetemp" name="usetemp" type="text" placeholder="e.g. High Temperature, Low Temperature" class="form-control input-md">
+                    <span class="help-block">If you are done with this printer, what temperature did you last use it for. If reserving, what temperature do you intend to use it for?</span>  
+                </div>
+            </div>
+
+            <!-- Submit Button -->
+            <div class="form-group">
+            <label class="col-md-4 control-label" for="submit"></label>
+                <div class="col-md-4">
+                    <button id="submit" name="submit" class="btn btn-primary">Update Printer</button>
+                </div>
+            </div>
+
+        </fieldset>
+    </form>
+
+    <!-- Links to other pages -->
+    <a href="gridlayout.php">Go to grid page</a><br>
+
+</body>
+
+</html>
---
+++ updateprinter.php
@@ -0,0 +1,59 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "usbw";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//take input from form and convert to variables
+$selected_printer = $_GET["selectedprinter"];
+$new_status = $_GET["newstatus"];
+$new_user = $_GET["newuser"];
+$use_temp = $_GET["usetemp"];
+
+$selected_printer = addslashes($selected_printer);
+$new_status = addslashes($new_status);
+$new_user = addslashes($new_user);
+$use_temp = addslashes($use_temp);
+
+
+//update status
+$sql = "UPDATE Printers_table SET Printer_status='$new_status' WHERE PrinterID LIKE '$selected_printer'";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Status for $selected_printer updated successfully";
+} else {
+  echo "Error updating printer status: " . $conn->error;
+}
+
+
+//update user
+$sql = "UPDATE Printers_table SET Printer_user='$new_user' WHERE PrinterID LIKE '$selected_printer'";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<br>User for $selected_printer updated successfully";
+} else {
+  echo "Error updating user: " . $conn->error;
+}
+
+
+//update temp
+$sql = "UPDATE Printers_table SET Printer_usetemp='$use_temp' WHERE PrinterID LIKE '$selected_printer'";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<br>Use temperature for $selected_printer updated successfully <br>";
+} else {
+  echo "Error updating use temperature: " . $conn->error;
+}
+
+
+$conn->close();
+?>
+
+<a href="gridlayout.php">Go to grid page to update</a>
