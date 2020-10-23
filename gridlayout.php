6b62d2d8b8d697f1ef3cfb216d6861291bc6e9dc
Jonathan Childers <jonathanc2015@gmail.com>
2020-10-22 22:45:24 -0500

    Deleted Extra Comment

--- gridlayout.php
+++ gridlayout.php
@@ -2,7 +2,6 @@
 
 <head>
 	<!-- Latest compiled and minified CSS -->
-	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
 	<!-- jQuery library -->
