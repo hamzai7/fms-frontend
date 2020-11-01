a94a03c615fd9523e2c3a69f6b4f8c0ae8aaf09f
hamzai7 <hamzaiqbal.hi7@gmail.com>
2020-11-01 00:06:58 -0500

    made form's database connection the same as grid's

--- updateprinter.php
+++ updateprinter.php
@@ -1,14 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "usbw";
-$dbname = "test";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+include "db_connect.php"; //connect to data base
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
 }
 
 //take input from form and convert to variables
@@ -26,34 +21,34 @@ $use_temp = addslashes($use_temp);
 //update status
 $sql = "UPDATE Printers_table SET Printer_status='$new_status' WHERE PrinterID LIKE '$selected_printer'";
 
-if ($conn->query($sql) === TRUE) {
+if ($mysqli->query($sql) === TRUE) {
   echo "Status for $selected_printer updated successfully";
 } else {
-  echo "Error updating printer status: " . $conn->error;
+  echo "Error updating printer status: " . $mysqli->error;
 }
 
 
 //update user
 $sql = "UPDATE Printers_table SET Printer_user='$new_user' WHERE PrinterID LIKE '$selected_printer'";
 
-if ($conn->query($sql) === TRUE) {
+if ($mysqli->query($sql) === TRUE) {
   echo "<br>User for $selected_printer updated successfully";
 } else {
-  echo "Error updating user: " . $conn->error;
+  echo "Error updating user: " . $mysqli->error;
 }
 
 
 //update temp
 $sql = "UPDATE Printers_table SET Printer_usetemp='$use_temp' WHERE PrinterID LIKE '$selected_printer'";
 
-if ($conn->query($sql) === TRUE) {
+if ($mysqli->query($sql) === TRUE) {
   echo "<br>Use temperature for $selected_printer updated successfully <br>";
 } else {
-  echo "Error updating use temperature: " . $conn->error;
+  echo "Error updating use temperature: " . $mysqli->error;
 }
 
 
-$conn->close();
+$mysqli->close();
 ?>
 
 <a href="gridlayout.php">Go to grid page to update</a>
