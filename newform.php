06f54eddabf5f6e6d9078f485e458c3ad2a26073
hamzai7 <hamzaiqbal.hi7@gmail.com>
2020-11-04 03:12:38 -0600

    added viewport and other meta tags

--- newform.php
+++ newform.php
@@ -1,6 +1,10 @@
-<html>
+<<!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Grid Page</title>
 
     <!-- Local CSS -->
     <link rel="stylesheet" href="css/styles.css">
--- newgridlayout.php
+++ newgridlayout.php
@@ -1,6 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Grid Page</title>
+
 	<!-- Local CSS -->
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/newstyles.css">
