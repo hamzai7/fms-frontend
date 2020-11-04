900e0bbc7fefa5db623fa9cb10e7dff08671ddb5
hamzai7 <hamzaiqbal.hi7@gmail.com>
2020-11-04 03:13:56 -0600

    corrected page title

--- newform.php
+++ newform.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Grid Page</title>
+    <title>New Form Page</title>
 
     <!-- Local CSS -->
     <link rel="stylesheet" href="css/styles.css">
